d4ddc5e40a2ef3ba1f280f0dfa7aa571091d2f83
Andrew Janke <andrew@apjanke.net>
2021-01-21 00:09:19 -0500

    Add java comment

--- src/java/fancyclip-java/src/main/java/net/janklab/fancyclip/DummyClipboardOwner.java
+++ src/java/fancyclip-java/src/main/java/net/janklab/fancyclip/DummyClipboardOwner.java
@@ -5,9 +5,9 @@ import java.awt.datatransfer.*;
 class DummyClipboardOwner implements ClipboardOwner {
 
   public DummyClipboardOwner() {
-
+    /* Empty because Matlab doesn't work with default constructors? So we have
+     * to explicitly supply a no-arg constructor? */
   }
-  
   
   public void lostOwnership(Clipboard clipboard, Transferable contents) {
     // NOP
